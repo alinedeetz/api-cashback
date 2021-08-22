0423abb5f8c75d2fc1d9296a50e313705f39cd08
Aline Mattos <alinesmattos@hotmail.com>
2021-08-22 15:30:34 -0300

    Create model 'Client'

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    adress = models.CharField(max_length=50)
+    email = models.EmailField(default='default@default.com')
