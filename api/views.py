30ece70c87fe916cad9c77ce641607323c9d25fb
Aline Mattos <alinesmattos@hotmail.com>
2021-08-22 15:33:15 -0300

    Create views for Clients, Products, Orders

--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets, generics
+from .models import Client, Product, Order
+from .serializers import CashbackSerializer, ClientSerializer, ProductSerializer, OrderSerializer
 
 # Create your views here.
+
+class ClientViewSet(viewsets.ModelViewSet):
+    serializer_class = ClientSerializer
+    queryset = Client.objects.all()
+
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+
+# class Cashback(viewsets.ModelViewSet):
+#     serialier_class = CashbackSerializer
